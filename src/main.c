1477c38f5365caf3bcac2d0683a89c0f0bf138cb
DeakinSimpson <deakinsimpson976@gmail.com>
2026-07-01 17:14:55 +1000

    Refactor main function to update argument handling; replace source and destination with latitude and longitude coordinates

--- src/main.c
+++ src/main.c
@@ -6,37 +6,39 @@
 #include "dijkstra.h"
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("usage: ./builder/pathfinder.exe <path_to_bin> <src> <dst>\n");
+    // initialise terminal variables
+    if (argc != 6) {
+        printf("usage: ./builder/pathfinder.exe <path_to_bin> <src lat> <src lon> <dst lat> <dst lon>\n");
         return 1;
     }
 
     printf("Pathfinder Starting...\n");
 
     const char* path = argv[1];
-    // long long src = atoll(argv[2]);
-    // long long dst = atoll(argv[3]);
+    Coordinate src_coord = {atof(argv[2]), atof(argv[3])};
+    Coordinate dst_coord = {atof(argv[4]), atof(argv[5])};
 
+    // initialise variables
     Graph* g = graph_load(path);
     HashMap* map = hashmap_create_index_from_graph(g);
     AdjList *adj = adjlist_create(g, map);
 
-    Coordinate src_coord = {45.47739253394642, 12.223124034279873};
-    Coordinate dst_coord = {45.546369611049755, 11.54529729703707};
-
     long long src_index = graph_nearest_node(g, src_coord, adj);
     long long dst_index = graph_nearest_node(g, dst_coord, adj);
 
-    ResultPath *rp = dijkstra(g, adj, map, g->nodes[src_index].id, g->nodes[dst_index].id);
-    if (rp == NULL) {
+    // run algorithms
+    ResultPath *dijkstra_rp = dijkstra(g, adj, map, g->nodes[src_index].id, g->nodes[dst_index].id);
+    if (dijkstra_rp == NULL) {
         printf("dFailed to find path\n");
     } else {
-        printf("travel time: %f minutes\ndistance travelled: %f kms\ntime to load: %f seconds\n", (rp->time_in_seconds / 60), (rp->distance_in_metres / 1000), rp->load_time_in_seconds);
+        printf("travel time: %f minutes\ndistance travelled: %f kms\ntime to load: %f seconds\n", (dijkstra_rp->time_in_seconds / 60), (dijkstra_rp->distance_in_metres / 1000), dijkstra_rp->load_time_in_seconds);
     }
     
+    // freeing variables
     adjlist_free(adj, g->node_count);
     hashmap_free(map);
     graph_free(g);
+    result_path_free(dijkstra_rp);
 
     printf("Terminating Program...\n");
     return 0;
